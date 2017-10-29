076f86da11fb620b5112bc3a4808e638bb264014
be3short <beshort@ucsc.edu>
2017-10-28 18:51:07 -0700

    Implemented menu instance class

---
+++ src/main/java/com/be3short/ui/menu/MenuInstance.java
@@ -0,0 +1,42 @@
+package com.be3short.ui.menu;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MenuInstance implements MenuDefinition
+{
+
+	private MenuDefinition[] subMenuItems;
+	private String label;
+
+	public MenuInstance(String label, MenuDefinition... sub_menus)
+	{
+		this.label = label;
+		if (sub_menus.length > 0)
+		{
+			addMenuDefinition(sub_menus);
+		}
+	}
+
+	@Override
+	public String label()
+	{
+		return label;
+	}
+
+	@Override
+	public MenuDefinition[] subMenuItems()
+	{
+		return subMenuItems;
+	}
+
+	public void addMenuDefinition(MenuDefinition... definitions)
+	{
+		ArrayList<MenuDefinition> newSubMenus = new ArrayList<MenuDefinition>(Arrays.asList(subMenuItems));
+		newSubMenus.addAll(Arrays.asList(definitions));
+		MenuDefinition[] newMenus = newSubMenus.toArray(new MenuDefinition[newSubMenus.size()]);
+		subMenuItems = newMenus;
+
+	}
+
+}
